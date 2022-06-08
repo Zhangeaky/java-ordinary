432056c5ce4949aea5479565791cb043cf4048f1
zhangeaky <zyk090104@163.com>
2022-06-08 21:17:09 +0800

    202206082117

---
+++ src/main/java/standardEdition/io/net/server/SimpleServer.java
@@ -0,0 +1,72 @@
+package standardEdition.io.net.server;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class SimpleServer {
+
+
+    public static void main(String[] args) throws Exception{
+
+        try{
+
+            ServerSocketChannel server = ServerSocketChannel.open();
+            server.bind(new InetSocketAddress(9090));
+            server.configureBlocking(false);
+
+            Set<SocketChannel> clients = new HashSet<>();
+
+            while (true) {
+
+                SocketChannel client = server.accept();
+                if (client != null) {
+                    System.out.println("remote address" + client.getRemoteAddress());
+                    client.configureBlocking(false);
+                    clients.add(client);
+                }
+
+
+                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+
+                for (SocketChannel element:clients) {
+                    System.out.println("文件描述符的数量是: " + clients.size());
+
+                    int read = element.read(byteBuffer);
+
+                    if ( read > 0) {
+
+                        byteBuffer.flip();
+                        System.out.println("read: " + read);
+                        byte[] aaa = new byte[byteBuffer.limit()];
+                        byteBuffer.get(aaa);
+                        String b = new String(aaa);
+                        System.out.println("content: " + b );
+                        byteBuffer.clear();
+
+                    }
+
+
+
+                }
+
+
+
+            }
+
+        }catch (IOException e) {
+
+        }
+
+
+
+    }
+
+}
